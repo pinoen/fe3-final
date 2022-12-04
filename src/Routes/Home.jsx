2df7fad6f66361fbe0c1baeae4a7ae0267da852c
Emilio Pino <pinoen@yahoo.com.ar>
2022-12-04 15:08:02 -0300

    se muestran cards en pagina home accediendo api de dentistas

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import Card from '../Components/Card'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Home = () => {
+
+  const [dentists, setDentist] = useState([])
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(res => setDentist(res.data))
+      .catch(err => console.log(err))
+  }, [])
+
+  console.log(dentists)
+
   return (
     <main className="" >
       <h1>Home</h1>
       <div className='card-grid'>
-        {/* Aqui deberias renderizar las cards */}
+        {dentists.map(dentist => (
+          <Card
+            key={dentist.id}
+            id={dentist.id}
+            name={dentist.name}
+            username={dentist.username}
+          />
+        ))}
       </div>
     </main>
   )
