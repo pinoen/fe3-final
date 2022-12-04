07216cdf85f486a3df4882afdaaa83a43d49441f
Emilio Pino <pinoen@yahoo.com.ar>
2022-12-04 12:56:25 -0300

    se realiza enrutamiento inicial en App.js con react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import Contact from './Routes/Contact'
+import Detail from './Routes/Detail'
+import Favs from './Routes/Favs'
+import Home from './Routes/Home'
 
 
 function App() {
   return (
-      <div className="App">
-          <Navbar/>
-          <Footer/>
-      </div>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/detail/:id" element={<Detail />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/destacados" element={<Favs />} />
+        </Routes>
+      </BrowserRouter>
+      <Navbar />
+      <Footer />
+    </div>
   );
 }
 
