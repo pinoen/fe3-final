95dd0f7dd05612a9ca97836baf7b2b780778b6d8
Emilio Pino <pinoen@yahoo.com.ar>
2022-12-04 22:35:42 -0300

    se agregan appbar de material ui

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppBar, Box, Button, IconButton, Toolbar, Typography } from '@mui/material'
+import NightlightIcon from '@mui/icons-material/Nightlight';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import { Link } from 'react-router-dom';
+// import { ContextGlobal } from './utils/global.context';
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
 
+  // const { state, dispatch } = useContext(ContextGlobal)
+
   return (
     <nav>
-      {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              DH Odonto
+            </Typography>
+            <Button color="inherit"><Link to={"/home"}>Home</Link></Button>
+            <Button color="inherit"><Link to={"/contact"}>Contact</Link></Button>
+            <Button color="inherit"><Link to={"/destacados"}>Favs</Link></Button>
+            <IconButton>
+              <NightlightIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </Box>
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
     </nav>
   )
 }
