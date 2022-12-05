9a7d4840f2590e814d2e8a4e3db3f7e56f3fcd32
Emilio Pino <pinoen@yahoo.com.ar>
2022-12-04 22:36:40 -0300

    se agrega imagen a footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import DHLogo from '../images/DH.png'
 
 const Footer = () => {
   return (
     <footer>
-        <p>Powered by</p>
-        <img src="./img/DH.png" alt='DH-logo' />
+      <p>Powered by</p>
+      <img src={DHLogo} alt='DH-logo' />
     </footer>
   )
 }
